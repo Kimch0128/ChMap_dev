28f5bb7514420833aabd35606c8c18254b51c122
KimChangHwan <kch@cowellfashion.co.kr>
2023-05-18 18:08:47 +0900

    1일차 - 기초개발
    #ChMap
    LinkedHashMap에 set을 put으로 변경하고 기본자료형(String,int,long,double) getter기능 개발
    
    #ChMapUtil
    비어있거나 null인지 확인하는 기능 개발

---
+++ chMap/src/main/java/com/chmap/chMap/ChMap.java
@@ -0,0 +1,78 @@
+package com.chmap.chMap;
+
+import org.apache.commons.lang3.StringUtils;
+
+import java.io.Serializable;
+import java.util.LinkedHashMap;
+import java.util.Set;
+
+public class ChMap extends LinkedHashMap<String, Object> implements Serializable{
+//	private static final long serialVersionUID = 4686410411375373904L;
+	
+	//LinkedHashMap set overwride
+	public boolean set(String key, Object value) {
+		boolean result = true;
+		try {
+			put(key,value);
+		} catch (Exception e) {
+			e.printStackTrace();
+			result = false;
+		}
+		return result;
+	}
+	
+	public String getString(String key) {
+		try {
+			return get(key).toString();
+		}catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+	
+	public int getInt(String key) {
+		int result;
+		try {
+			return Integer.parseInt(get(key).toString());
+		}catch (Exception e) {
+			e.printStackTrace();
+			return (Integer) null;
+		}
+	}
+	
+	public long getLong(String key) {
+		try {
+			return Long.parseLong(get(key).toString());
+		} catch (Exception e) {
+			e.printStackTrace();
+			return (Long) null;
+		}
+	}
+	
+	public double getDouble(String key) {
+		try {
+			return Double.parseDouble(get(key).toString());
+		} catch (Exception e) {
+			e.printStackTrace();
+			return (Double) null;
+		}
+	}
+//	public boolean addMap(ChMap chMap) {
+//		boolean result = true;
+//		try {
+//			chMap.forEach((k,v)->{
+//				boolean isExists = containsKey(k);
+//				
+//				if(!isExists)
+//					set(k,v);
+//				else
+//					set("dup_"+k,v);
+//			});
+//		} catch (Exception e) {
+//			// TODO: handle exception
+//			e.printStackTrace();
+//			result = false;
+//		}
+//		return result;
+//	}
+}
---
+++ chMap/src/main/java/com/chmap/chMap/ChMapUtil.java
@@ -0,0 +1,10 @@
+package com.chmap.chMap;
+
+public class ChMapUtil{
+
+	public static boolean isEmpty(ChMap chMap) {
+		if(chMap == null || chMap.isEmpty())
+			return true;
+		return false;
+	}
+}
