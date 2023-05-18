93d64bebcefc73a01c09189047dac191aa60ad08
KimChangHwan <kch@cowellfashion.co.kr>
2023-05-18 18:10:08 +0900

    .
    .

--- chMap/src/main/java/com/chmap/chMap/App.java
+++ chMap/src/main/java/com/chmap/chMap/App.java
@@ -1,5 +1,7 @@
 package com.chmap.chMap;
 
+import java.util.LinkedHashMap;
+
 /**
  * Hello world!
  *
@@ -8,6 +10,26 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	ChMap map = null;
+        
+    	System.out.println(ChMap.isEmpty(map));
+    	System.out.println(map.isEmpty(map));
+        
+//        for(int i=97; i<123; i++) {
+//        	String key = Character.toString((char)i);
+//        	map.put(key, key.toUpperCase());
+//        }
+        
+//        System.out.println(map.get("123").toString());
+        
+//        System.out.println(Integer.parseInt(" "));
+        
+//        System.out.println(map.toString());
+//        map.forEach((k,v)->{
+//        	System.out.print(k);
+//        	System.out.print(":");
+//        	System.out.println(v);
+//        	});
+        
     }
 }
