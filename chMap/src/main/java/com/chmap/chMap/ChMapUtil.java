f7f8319799eb5e0c5df1e48b116144009f8b5ac5
Kimch0128 <80230171+Kimch0128@users.noreply.github.com>
2023-05-22 10:53:47 +0900

    arrayToArrayList 추가
    배열을 arrayList로 변경

--- chMap/src/main/java/com/chmap/chMap/ChMapUtil.java
+++ chMap/src/main/java/com/chmap/chMap/ChMapUtil.java
@@ -1,10 +1,17 @@
 package com.chmap.chMap;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class ChMapUtil{
 
 	public static boolean isEmpty(ChMap chMap) {
 		if(chMap == null || chMap.isEmpty())
 			return true;
 		return false;
+	}
+	
+	public static <E> ArrayList<E> arrayToArrayList(E[] array){
+		return new ArrayList<E>(Arrays.asList(array));
 	}
 }
